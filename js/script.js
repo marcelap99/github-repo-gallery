4d6d218b4bcc968b39f8d63b5520e894d44346b3
Marcela Pino <marcelampino2018@gmail.com>
2021-12-01 16:36:57 -0500

    Completed second step of github repo gallery project.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 // GRAB HTML ELEMENTS
 const overviewDiv = document.querySelector(".overview");
-
+const repoList = document.querySelector(".repo-list");
 // GLOBAL VARIABLES
 const username = "marcelap99";
 
-console.log(overviewDiv);
+// console.log(overviewDiv);
 
 // CREATE ASYNC FUNCTION TO FETCH DATA FROM GIT HUB API
 const getData = async function(){
@@ -12,12 +12,13 @@ const getData = async function(){
   const fetchRequest = await fetch(`https://api.github.com/users/${username}`);
      // resolve the JSON response
   const jsonData = await fetchRequest.json();
-console.log(jsonData);
+// console.log(jsonData);
 
 displayData(jsonData);
 };
 
 getData();
+
 
 // Create function to display the fetched user information on the page.
 const displayData = function(jsonData){
@@ -37,5 +38,39 @@ const displayData = function(jsonData){
     </div> `;
 // Insert new div into display div in HTML
     overviewDiv.append(newDiv);
+
+    // call getRepo async function
+    getRepos();
+};
+
+
+// create and name a new async function to fetch your repos
+const getRepos = async function(){
+
+// fetch request with parameters for sort & 100 per page
+  const repoRequest = await fetch(`https://api.github.com/users/${username}/repos?sort=created&per_page=100`);
+
+    // resolve the JSON response
+ const jsObj = await repoRequest.json();
+// console.log(jsObj);
+
+// call display repo function pass in json response
+displayRepos(jsObj);
+};
+
+
+// create and name a function to display information about each repo.
+const displayRepos = function(repos){
+  // loop through array of objects
+  for(const repo of repos){
+    // create a li for each repo
+    const repoLi = document.createElement("li");
+    // give new li class name "repo"
+    repoLi.classList.add("repo");
+    // insert h3 with placeholder for each repo name
+    repoLi.innerHTML = `<h3>${repo.name}</h3>`;
+    // add new li to ul already visible in doc body
+    repoList.append(repoLi);
+  }
 
 };
